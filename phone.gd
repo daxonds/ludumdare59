f3358b85d4ba0a6ea33e1d499f5fe1532e2b6ec8
daxonds <your@email.com>
2026-04-18 02:19:56 -0500

    added password unlock game

--- phone.gd
+++ phone.gd
@@ -4,14 +4,16 @@ signal finished
 
 const RADIUS = 30
 # TODO: password
-const GAMES = ["dots", "pattern"]
+const GAMES = ["dots", "pattern", "password"]
 
 @export var phone_position: Vector2 = Vector2(50, 50)
 @export var phone_size: Vector2 = Vector2(400, 500)
 
 func _ready() -> void:
 	# Randomly load mini game
-	var game_name = GAMES[randi() % GAMES.size()]
+	# var game_name = GAMES[randi() % GAMES.size()]
+	# uncomment above for randomization. comment or delete below afterwards
+	var game_name = GAMES[2]
 	var script = load("res://%s.gd" % game_name)
 	var game = Node2D.new()
 	game.set_script(script)
